ef57de202e7393b6ddb01f902325a1ebbbae7ac4
Behzod Halil <behzoddev@gmail.com>
2023-04-03 18:36:49 +0900

    Add assertion functions for characters

---
+++ osmon-android/src/main/java/io/behzod/osmonandroid/CharSequence.kt
@@ -0,0 +1,61 @@
+package io.behzod.osmonandroid
+
+infix fun <T : CharSequence> T.toStartWith(expected: T): T {
+  return this.apply {
+    verify("", this.startsWith(expected))
+  }
+}
+
+infix fun <T : CharSequence> T.toEndWith(expected: T): T {
+  return this.apply {
+    verify("", this.endsWith(expected))
+  }
+}
+
+infix fun <T : CharSequence> T.toContain(character: Char): T {
+  return this.apply {
+    verify("", this.contains(character))
+  }
+}
+
+infix fun <T : CharSequence> T.notToContain(characters: Iterable<CharSequence>): T {
+  return this.apply {
+    verify("", characters.none { character -> this.contains(character) })
+  }
+}
+
+fun <T : CharSequence> T.notToContain(vararg expected: CharSequence): T {
+  return this.apply {
+    this.notToContain(expected.toList())
+  }
+}
+
+
+infix fun <T : CharSequence> T.match(regex: String): T {
+  return this.apply {
+    verify("", this.matches(Regex(regex)))
+  }
+}
+
+infix fun <T : CharSequence> T.match(regex: Regex): T {
+  return this.apply {
+    verify("", this.matches(regex))
+  }
+}
+
+fun <T : CharSequence> T.toBeBlank(): T {
+  return this.apply {
+    verify("", this.isBlank())
+  }
+}
+
+fun <T : CharSequence> T.toBeEmpty(): T {
+  return this.apply {
+    verify("", this.isEmpty())
+  }
+}
+fun <T: CharSequence> T.toBeNull(): T {
+  return this.apply {
+    verify("", !this.isNullOrEmpty())
+  }
+}
---
+++ osmon-android/src/test/java/io/behzod/osmonandroid/ExampleUnitTest.kt
@@ -0,0 +1,24 @@
+package io.behzod.osmonandroid
+
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+  @Test
+  fun addition_isCorrect() {
+    val expect = ""
+
+    expect.toBeBlank()
+  }
+
+  @Test
+  fun checkIfCharacterIsEmpty() {
+    val expect: String? = null
+
+    expect?.toBeNull()
+  }
+}
