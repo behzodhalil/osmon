324575a084763009e403b50e0c30fc2428675594
Behzod Halil <behzoddev@gmail.com>
2023-04-18 16:15:17 +0900

    Add custom mismatch exception

---
+++ osmon-core/src/main/java/io/behzod/osmoncore/exception/MismatchException.kt
@@ -0,0 +1,11 @@
+package io.behzod.osmoncore.exception
+
+class MismatchException(
+  message: String?, private val expected: String?, private val actual: String?
+) : RuntimeException(
+  """${message ?: ""}
+        |Expected: <$expected> but was: <$actual>
+    """.trimMargin().trim()
+) {
+  constructor(message: String?, expected: Any?, actual: Any?) : this(message, expected.toString(), actual.toString())
+}
